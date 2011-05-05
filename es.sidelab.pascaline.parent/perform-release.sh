6b5557ffebd9adc31f7e0d1779991b48ffa67e89
Patxi Gortázar <patxi.gortazar@gmail.com>
2011-05-05 10:28:46 +0000

    Added release script.

---
+++ es.sidelab.pascaline.parent/perform-release.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+if [ $# -ne 4 ]
+then
+  echo "Usage: `basename $0` {tagVersion} {nextVersion} {username} {password}"
+  echo "Run this script from the parent project"
+  exit
+fi
+
+timestamp=`date -u +%Y%m%d%H%M`
+tagVersion=$1
+# With this approach, p2 generation fails saying that version-timestamp is not a
+# valid sustitution pattern for version.qualifier
+#releaseVersion=$tagVersion-$timestamp
+releaseVersion=$tagVersion.$timestamp
+nextVersion=$2.qualifier
+
+echo "tagVersion=$tagVersion"
+echo "releaseVersion=$releaseVersion"
+echo "nextVersion=$nextVersion"
+echo "username=$3"
+#echo -e "Are you sure (y/N)? \c "
+#read confirm
+#if [ "-"$confirm != "-y" ]; then
+#  exit
+#fi
+
+export M2_HOME=/home/usuario/maven3
+export M2=$M2_HOME/bin
+export MAVEN_OPTS=-Dorg.apache.maven.global-settings=/home/usuario/maven3/conf/settings.xml
+export PATH=$PATH:$M2
+
+cd trunk/es.sidelab.pascaline.parent
+
+# Prepare release versions
+mvn org.sonatype.tycho:tycho-versions-plugin:set-version -Dtycho.mode=maven -DnewVersion=$releaseVersion
+
+# Perform build with the release version
+mvn clean deploy
+
+# Commit changes (preparing for tagging)
+cd ..
+svn ci -m "prepare release pascaline-ide-$tagVersion" --username $3 --password $4
+
+# Tag release
+svn copy https://code.sidelab.es/svn/pascaline/ide/trunk/ https://code.sidelab.es/svn/pascaline/ide/tags/pascaline-ide-$tagVersion -m "copy for tag pascaline-ide-$tagVersion" --username $3 --password $4
+
+# Prepare next development version
+cd es.sidelab.pascaline.parent
+mvn org.sonatype.tycho:tycho-versions-plugin:set-version -Dtycho.mode=maven -DnewVersion=$nextVersion
+
+# Commit changes to trunk (trunk now contains the new development version)
+cd ..
+svn ci -m "prepare for next development iteration" --username $3 --password $4
+
+echo "Done!"
